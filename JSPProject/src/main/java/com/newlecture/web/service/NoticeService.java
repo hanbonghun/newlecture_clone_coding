964d5e3fe36299dfcc3894b0bd58f9b4bda1f988
hanbonghun <hanbonghun@TFX5470H>
2022-03-08 03:40:18 +0900

    전체 삭제, 공개
    pub가 1인 (공개) 리스트 출력 함수

--- JSPProject/src/main/java/com/newlecture/web/service/NoticeService.java
+++ JSPProject/src/main/java/com/newlecture/web/service/NoticeService.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -13,9 +14,99 @@ import com.newlecture.web.entitiy.Notice;
 import com.newlecture.web.entitiy.NoticeView;
 
 public class NoticeService {
+	public int deleteNoticeAll(int[] ids) {   //모든 공지사항 제거 
+		int result= 0; //삭제 개수 
+		String params = "";
+		
+		for(int i=0; i<ids.length; i++) {
+			params +=ids[i];
+			if(i<ids.length-1)
+				params +=",";
+		}
+		String sql = 
+				"DELETE NOTICE WHERE ID IN (" +params+ ") ";
+		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+		String uid = "newlec";
+		String pwd = "nexon0918,";
+		Notice notice = null;
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection(url, uid, pwd);
+			Statement st = con.createStatement();
+			
+
+			result = st.executeUpdate(sql);
+
 	
+			st.close();
+			con.close();
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+		return result;
 	
-	public List<NoticeView> getNoticeList(){
+	}
+	
+	public int pubNoticeAll(int ids) {
+		return 0; 
+	}
+	
+	public int insertNotice(Notice notice) { //공지사항 입력 
+		
+		int result= 0; //삭제 개수 
+		
+		String sql = 
+				"INSERT INTO NOTICE(TITLE, CONTENT,WRITER_ID, PUB, FILES) VALUES (?,?,?,?,?) ";
+		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+		String uid = "newlec";
+		String pwd = "nexon0918,";
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection(url, uid, pwd);
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, notice.getTitle());
+			st.setString(2, notice.getContent());
+			st.setString(3, notice.getWriter());
+			st.setBoolean(4, notice.getPub());
+			st.setString(5, notice.getFiles());
+			
+			result = st.executeUpdate();
+
+	
+			st.close();
+			con.close();
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+		return result;
+	}
+	
+	public int deleteNotice(int id) {
+		return 0; 
+	}
+	
+	public int updateNotice(Notice notice) {
+		return 0; 
+	}
+	
+//	public List<Notice> getNoticeNewestList(){
+//		return getNoticeList("","",1);
+//	}
+	public List<NoticeView> getNoticeViewList(){
 		return getNoticeList("title","",1); 
 	}
 	
@@ -48,7 +139,8 @@ public class NoticeService {
 				String hit = rs.getString("HIT");
 				String files = rs.getString("FILES");
 				int cmtCount = rs.getInt("CMT");
-				NoticeView notice = new NoticeView(id, title, writer_id, regdate, hit, files, cmtCount);
+				boolean pub = rs.getBoolean("PUB");
+				NoticeView notice = new NoticeView(id, title, writer_id, regdate, hit, files,pub, cmtCount);
 				list.add(notice);
 
 			}
@@ -128,8 +220,8 @@ public class NoticeService {
 				String hit = rs.getString("HIT");
 				String files = rs.getString("FILES");
 				String content = rs.getString("CONTENT");
-
-				notice = new Notice(_id, title, writer_id, regdate, hit, files, content);
+				boolean pub = rs.getBoolean("PUB");
+				notice = new Notice(_id, title, writer_id, regdate, hit, files, content,pub);
 
 			}
 
@@ -175,8 +267,8 @@ public class NoticeService {
 				String hit = rs.getString("HIT");
 				String files = rs.getString("FILES");
 				String content = rs.getString("CONTENT");
-
-				notice = new Notice(_id, title, writer_id, regdate, hit, files, content);
+				boolean pub = rs.getBoolean("PUB");
+				notice = new Notice(_id, title, writer_id, regdate, hit, files, content,pub);
 
 			}
 
@@ -222,8 +314,8 @@ public class NoticeService {
 				String hit = rs.getString("HIT");
 				String files = rs.getString("FILES");
 				String content = rs.getString("CONTENT");
-
-				notice = new Notice(_id, title, writer_id, regdate, hit, files, content);
+				boolean pub = rs.getBoolean("PUB");
+				notice = new Notice(_id, title, writer_id, regdate, hit, files, content,pub);
 
 			}
 
@@ -240,5 +332,50 @@ public class NoticeService {
 		}
 				
 		return notice;
+	}
+
+	public List<NoticeView> getPubNoticeList(String field, String query, int page) {
+		String sql = "SELECT *  FROM (SELECT ROWNUM RN,N.* FROM (SELECT * FROM NOTICE_VIEW WHERE "+field+ " LIKE ? AND PUB=1 ORDER BY REGDATE DESC) N )  WHERE RN BETWEEN ? AND ?";
+		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+		String uid = "newlec";
+		String pwd = "nexon0918,";
+		
+		List<NoticeView>list  = new ArrayList<>();
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection(url, uid, pwd);
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, "%"+query+"%");
+			st.setInt(2, 1+ (page-1)*10);
+			st.setInt(3, page*10);
+
+			ResultSet rs = st.executeQuery();
+
+			while (rs.next()) {
+				int id = rs.getInt("ID");
+				String title = rs.getString("TITLE");
+				String writer_id = rs.getString("WRITER_ID");
+				Date regdate = rs.getDate("REGDATE");
+				String hit = rs.getString("HIT");
+				String files = rs.getString("FILES");
+				int cmtCount = rs.getInt("CMT");
+				boolean pub = rs.getBoolean("PUB");
+				NoticeView notice = new NoticeView(id, title, writer_id, regdate, hit, files,pub, cmtCount);
+				list.add(notice);
+
+			}
+
+			rs.close();
+			st.close();
+			con.close();
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list;
 	}
 }
