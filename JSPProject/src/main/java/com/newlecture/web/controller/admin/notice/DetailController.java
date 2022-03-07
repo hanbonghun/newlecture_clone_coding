5adde0cd83d64a7b5b4de092a92d852c5598b443
hanbonghun <hanbonghun@TFX5470H>
2022-03-08 03:37:46 +0900

    글 자세히 보기 controller

---
+++ JSPProject/src/main/java/com/newlecture/web/controller/admin/notice/DetailController.java
@@ -0,0 +1,29 @@
+package com.newlecture.web.controller.admin.notice;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.newlecture.web.entitiy.Notice;
+import com.newlecture.web.service.NoticeService;
+
+
+@WebServlet("/admin/board/notice/detail")
+public class DetailController extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String _id = req.getParameter("id");
+		int id = Integer.parseInt(_id);
+		Notice notice =null;
+		NoticeService service = new NoticeService();
+		notice = service.getNotice(id);
+		req.setAttribute("n", notice);
+		//fowward : detail.jsp도 쓸 수 있게 req,resp 객체를 통해 
+		req.getRequestDispatcher("/WEB-INF/view/admin/board/notice/detail.jsp").forward(req, resp);
+		
+	}
+}	
